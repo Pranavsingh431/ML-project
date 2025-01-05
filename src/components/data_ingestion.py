46ab6d5d764b2a5897b687c02ff67e6a54b9d216
Pranavsingh431 <singhpranav431@gmail.com>
2025-01-05 11:23:51 +0530

    model training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -2,11 +2,14 @@ import os
 import sys
 from src.exception import CustomException
 from src.logger import logging
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
 import pandas as pd 
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', 'train.csv')
@@ -38,9 +41,16 @@ class DataIngestion:
             )
             
         except Exception as e:
-            raise CustomException(e, sys)
+            raise CustomException(str(e), sys)
 
 
 if __name__ == '__main__':
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+    
+    data_transformation = DataTransformation()
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+    
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
+    
---
+++ src/utils.py
@@ -0,0 +1,50 @@
+import os 
+import sys
+import dill
+import pickle
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
+from src.exception import CustomException
+from src.logger import logging
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        
+        with open(file_path, 'wb') as file_obj:
+            pickle.dump(obj, file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def evaluate_model(X_train, y_train, X_test, y_test, models, param):
+    try:
+        report = {}
+        
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            model_name = list(models.keys())[i]
+            
+            para = param.get(model_name, {})
+
+            grid = GridSearchCV(model, para, cv=3)
+            grid.fit(X_train, y_train)
+            
+            model.set_params(**grid.best_params_)
+            model.fit(X_train, y_train)
+            
+            y_pred_train = model.predict(X_train)
+            y_pred_test = model.predict(X_test)
+            
+            train_model_score = r2_score(y_train, y_pred_train)
+            test_model_score = r2_score(y_test, y_pred_test)
+
+            report[model_name] = test_model_score
+        
+        return report   
+        
+    except Exception as e:
+        raise CustomException(e, sys)
